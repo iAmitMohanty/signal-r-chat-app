21dbc81b666fe88fb4551e5389fdf246be22c75c
Amit Mohanty <amit-dotnet@webappclouds.com>
2023-08-09 16:24:41 +0530

    Add project files.

---
+++ SignalRChatApp/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignalRChatApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+
+    }
+}
